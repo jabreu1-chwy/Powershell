c60231ede9497c87ccdcb98171789b0c34d285b4
Joel Abreu <87429916+jabreu1-chwy@users.noreply.github.com>
2023-05-26 07:15:54 -0700

    Add files via upload

---
+++ bulk_delete_dls_onprem.ps1
@@ -0,0 +1,22 @@
+﻿# Specify the path to the text file containing distribution list names
+$listFile = "C:\Users\AA-JAbreu1.001\Documents\lists.txt"
+
+# Read the list of distribution list names from the file
+$lists = Get-Content $listFile
+
+# Loop through each distribution list and delete it
+foreach ($list in $lists) {
+    try {
+        $distributionList = Get-ADGroup -Filter {Name -eq $list -and GroupCategory -eq 'Distribution'}
+        if ($distributionList) {
+            Remove-ADGroup -Identity $distributionList.DistinguishedName -Confirm:$false
+            Write-Host "Deleted distribution list: $list" -ForegroundColor Cyan
+        } else {
+            Write-Host "Distribution list not found: $list" -ForegroundColor Red
+        }
+    }
+    catch {
+        Write-Host "Error deleting distribution list: $list" -ForegroundColor Red
+        Write-Host $_.Exception.Message
+    }
+}
